3e4865b0ea2ba1aa4cdeeda2f90d9dc81d2d5d5a
Milan Junges <junges.milan22@gmail.com>
2026-03-23 11:34:52 -0400

    ajout de reset des index des images des echos du mur

--- src/functions/echosImages.js
+++ src/functions/echosImages.js
@@ -1,20 +1,15 @@
 const { resetIndexes } = require("../controllers/echosImagesController");
 const pool = require("../models/db");
 
-getByColor = async (color) => {
+exports.getByColor = async (color) => {
     return await pool.query(`
             SELECT * FROM echos_images WHERE color = $1;
         `, [color]);
 }
 
-resetIndexes = async () => {
+exports.resetIndexes = async () => {
     return await pool.query(`
         UPDATE echos_images
         SET index = 0
         RETURNING *`)
-}
-
-module.exports = {
-    getByColor,
-    resetIndexes
 }
